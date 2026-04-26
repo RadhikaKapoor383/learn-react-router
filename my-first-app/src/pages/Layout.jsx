816fbf56f2a131378f5f7a237efd9d3e5f973e8e
Radhika kapoor <radhikakapoor383@gmail.com>
2026-04-27 00:55:17 +0500

    Add Layout component to structure page with Navbar and Outlet

---
+++ my-first-app/src/pages/Layout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+function Layout() {
+    return (
+        <>
+            <Navbar /> {/* Always visible at top */}
+            <Outlet /> {/* Child page renders HERE */}
+            {/* <Footer /> would go here too */}
+        </>
+    );
+}
+export default Layout;
