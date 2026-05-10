64995ec428967773271e2e19e5daa9d3fdde0c6e
Radhika kapoor <radhikakapoor383@gmail.com>
2026-05-11 02:17:45 +0500

    import Link

--- route-params/src/SearchParam.jsx
+++ route-params/src/SearchParam.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { useSearchParams } from 'react-router-dom';
 import Products from './public/Products.jsx';
 
