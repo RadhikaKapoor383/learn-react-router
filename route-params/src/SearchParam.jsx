2c263cb085201e8fe77f6eb9208b8f25330af390
Radhika kapoor <radhikakapoor383@gmail.com>
2026-05-14 01:47:15 +0500

    update searchParams.jsx file

--- route-params/src/SearchParam.jsx
+++ route-params/src/SearchParam.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { useSearchParams } from 'react-router-dom';
+import Home from './Home.jsx';
 import Products from './public/Products.jsx';
+import './SearchParam.css';
 
 function SearchParam() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/products?category=shoes&color=red">Products Page</Link>
+      <nav className="navbar">
+        <Link className="brand" to="/">Search Params</Link>
+        <Link to="/products?category=shoes&color=red&size=medium&price=low&sort=asc">Products Page</Link>
       </nav>
 
       <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
-        <Route path="*" element={<h1>404 Not Found</h1>} />
+        <Route path="*" element={<h1 className="not-found">404 Not Found</h1>} />
       </Routes>
     </BrowserRouter>
   );
