99d0aef4a7f9e6a053ed0f934961597b06848f32
Radhika kapoor <radhikakapoor383@gmail.com>
2026-05-14 01:56:10 +0500

    the searchParam.jsx file updates because of ongoing changes as things added

--- route-params/src/SearchParam.jsx
+++ route-params/src/SearchParam.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import About from './About.jsx';
+import Contact from './Contact.jsx';
 import Home from './Home.jsx';
 import Products from './public/Products.jsx';
 import './SearchParam.css';
@@ -8,12 +10,19 @@ function SearchParam() {
     <BrowserRouter>
       <nav className="navbar">
         <Link className="brand" to="/">Search Params</Link>
-        <Link to="/products?category=shoes&color=red&size=medium&price=low&sort=asc">Products Page</Link>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/products?category=shoes&color=red&size=medium&price=low&sort=asc">Products</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<h1 className="not-found">404 Not Found</h1>} />
       </Routes>
     </BrowserRouter>
